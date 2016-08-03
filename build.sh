52c7117ed453ffa41f64d0efe47e3f6c256bcd67
mridsole <mridsole@gmail.com>
2016-08-03 19:27:34 +1000

    ???

--- build.sh
+++ build.sh
@@ -1,17 +1,26 @@
 #! /bin/bash
 
+compiler="g++-5"
 libname=libchw.so
 links="-lsfml-graphics -lsfml-window -lsfml-system"
 
 compile_options="-O3 -std=c++11 -Wall -Werror $links"
 
+# make folders if necessary
+mkdir -p bin
+mkdir -p lib
+mkdir -p obj
+
+# remove ctest object file if necessary ...
+rm obj/ctest.o 2> /dev/null
+
 # compile each file 
 shopt -s nullglob
 for f in src/c/*.cpp; do
     bname=$(basename $f)
-    g++-5 -c -fPIC $compile_options $f -o obj/"${bname%.*}.o" | tee /dev/tty
+    $compiler -c -fPIC $compile_options $f -o obj/"${bname%.*}.o" | tee /dev/tty
 done
 
 # link to make a shared lib
 obj_files=obj/*.o
-g++-5 -shared -fPIC $obj_files -o lib/$libname | tee /dev/tty
+$compiler -shared -fPIC $obj_files -o lib/$libname | tee /dev/tty
--- src/jl/test.jl
+++ src/jl/test.jl
@@ -6,7 +6,7 @@ Libdl.dlopen("lib/libchw", Libdl.RTLD_GLOBAL)
 include("sfml.jl")
 include("terminal.jl")
 
-const term_dims = (160, 40)
+const term_dims = (160, 140)
 
 # TODO: make some kind of julia wrapper for this
 const hwgame = ccall((:hwGame_create, "lib/libchw"), Ptr{Void}, (Vector2u, Cstring, Vector2i, Cstring, Cint), 
@@ -14,12 +14,22 @@ const hwgame = ccall((:hwGame_create, "lib/libchw"), Ptr{Void}, (Vector2u, Cstri
 
 const term = Terminal(term_dims[1], term_dims[2], hwgame)
 
+# set random colors
+for i in eachindex(term.bg_colors) 
+    term.bg_colors[i] = Color(rand(UInt8), rand(UInt8), rand(UInt8)) 
+end
+
 function tick_game(x)
 
-    # slightly darken
-    for i = 1:term_dims[1], j = 1:term_dims[2]
-        term.ch_colors[i, j] -= 8
+    for i in eachindex(term.bg_colors) 
+        term.bg_colors[i] = Color(0x00, rand(UInt8), 0x00) * 0.4
+        term.chs[i] = rand(UInt8)
     end
+
+    # slightly darken
+    #for i = 1:term_dims[1], j = 1:term_dims[2]
+    #    term.bg_colors[i, j] -= 8
+    #end
     
     # set the characters, background colors and char colors
     update(term)
